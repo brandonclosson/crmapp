7d82aae3e26882a54798008cb4a3d49e51c24997
brandonclosson <bclosson@gmail.com>
2015-01-16 14:38:12 -0500

    added Heroku settings to Django

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -41,7 +41,7 @@ if ENV_ROLE == 'development':
     TEMPLATE_DEBUG = DEBUG
     CRMEASY_DB_PASS = get_env_variable('CRMEASY_DB_PASS')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,3 +125,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
     'django.core.context_processors.request',
 )
+
+if ENV_ROLE == 'production':
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
