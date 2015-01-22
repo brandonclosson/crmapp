06ed90facb1d62fab804e56863dba36a2df4836b
Brandon Closson <bclosson@gmail.com>
2015-01-22 14:25:44 -0500

    created account list view

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -1,36 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from django.utils.decorators import method_decorator
 
-from django.db import models
-from django.contrib.auth.models import User
+from .models import Account
 
-from shortuuidfield import ShortUUIDField
+class AccountList(ListView):
+	model = Account
+	template_name = 'accounts/account_list.html'
+	context_object_name = 'accounts'
 
-class Account(models.Model):
-	uuid = ShortUUIDField(unique=True)
-	name = models.CharField(max_length=80)
-    desc = models.TextField(blank=True)
-    address_one = models.CharField(max_length=100)
-    address_two = models.CharField(max_length=100, blank=True)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=2)
-    phone = models.CharField(max_length=20)
-    owner = models.ForeignKey(User)
-    created_on = models.DateField(auto_now_add=True)
+	def get_queryset(self):
+		account_list = Account.objects.filter(owner=self.request.user)
+		return account_list
 
-    class Meta:
-    	verbose_name_plural = 'accounts'
-
-    def __unicode__(self):
-    	return u"%s" % self.name
-
-    @models.permalink
-    def get_absolute_url(self):
-    	return 'account_detail', [self.uuid]
-
-    @models.permalink
-    def get_update_url(self):
-    	return 'account_update', [self.uuid]
-
-    @models.permalink
-    def get_delete_url(self):
-    	return 'account_delete', [self.uuid]
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super(AccountList, self).dispatch(*args, **kwargs)
