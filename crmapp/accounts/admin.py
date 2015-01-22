aaa7500ebed1ccf25c13ceeb8d67c196b4a22e5b
Brandon Closson <bclosson@gmail.com>
2015-01-22 14:46:50 -0500

    fixed some typos and added a custom admin action to accounts for cloning

--- crmapp/accounts/admin.py
+++ crmapp/accounts/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Account
 
-admin.site.register(Account)
+def make_clone(modeladmin, request, queryset):
+	for obj in queryset:
+		account = Account(name=obj.name, phone=obj.phone, address_one=obj.address_one,
+						  city=obj.city, state=obj.state, owner=obj.owner)
+		account.save()
+
+class AccountAdmin(admin.ModelAdmin):
+	actions = [make_clone]
+
+
+admin.site.register(Account, AccountAdmin)
