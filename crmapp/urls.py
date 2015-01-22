a0c897d33941cf544a20eb43d8c6038032f04aa4
Brandon Closson <bclosson@gmail.com>
2015-01-22 14:19:03 -0500

    created account list URL

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -17,5 +17,10 @@ urlpatterns = patterns('',
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login/'}),
 
+    # Account related URLs
+    url(r'^account/list/$',
+    	AccountList.as_view(), name="account_list"
+    ),
+
     (r'^admin/', include(admin.site.urls)),
 )
